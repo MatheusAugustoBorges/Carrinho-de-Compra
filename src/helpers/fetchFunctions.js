a6fa4e1cc184e06dac6ad4fb098456250ae3f9d7
Matheus Augusto <matheus_augusto92@hotmail.com>
2023-04-25 15:58:01 -0300

    requisito 1 v2.0

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -4,7 +4,7 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = async (product) => {
+export const fetchProductsList = async () => {
   // // const response = await fetch(`${API_URL}${product}`);
   // const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
   // console.log(response);
