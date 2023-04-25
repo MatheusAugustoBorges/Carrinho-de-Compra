4782b7c01a566f4236b507e0aab7e7da2c28218f
Matheus Augusto <matheus_augusto92@hotmail.com>
2023-04-25 17:05:17 -0300

    requisito 2 v2.0

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
-import { fetchProductsList } from './helpers/fetchFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
