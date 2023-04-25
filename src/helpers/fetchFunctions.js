c4fc0d4a65d15e750071b05e4631a2570ca18e19
Matheus Augusto <matheus_augusto92@hotmail.com>
2023-04-25 15:55:13 -0300

    requisito 1

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,7 +1,16 @@
-export const fetchProduct = () => {
-  // seu código aquii
-};
+// const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=';
 
-export const fetchProductsList = () => {
+export const fetchProduct = () => {
   // seu código aqui
 };
+
+export const fetchProductsList = async (product) => {
+  // // const response = await fetch(`${API_URL}${product}`);
+  // const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
+  // console.log(response);
+  // const data = await response.json();
+  // console.log(data.site_id);
+  // return data.results;
+};
+
+// fetchProductsList('computador');
