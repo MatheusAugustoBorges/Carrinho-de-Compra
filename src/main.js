ac0a716d16d5c912be67c941ff336e921d70b2df
Matheus Augusto <matheus_augusto92@hotmail.com>
2023-04-25 19:10:38 -0300

    requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
+
+const productElement = document.querySelector('.products');
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const productResults = await fetchProductsList('computador');
+
+const featuresProduct = productResults.map((product) => {
+  const add = createProductElement(product);
+  return productElement.appendChild(add);
+});
